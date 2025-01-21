c4670fe0ccd9d83c81ec9357573d9991ee0de01c
王哲玮 <m13636472690@163.com>
2025-01-21 15:02:37 -0500

    fix route

--- app/main/map/page.tsx
+++ app/main/map/page.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Map() {
   return (
-    <div>
-      <h1>Map</h1>
+    <div className="p-4">
+      {/* Header */}
+      <div className="flex mb-6">
+        <h1 className="text-3xl font-bold">Map</h1>
+      </div>
     </div>
   );
 }
--- app/main/schedule/page.tsx
+++ app/main/schedule/page.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Schedule() {
   return (
-    <div>
-      <h1>Schedule</h1>
+    <div className="p-4">
+      {/* Header */}
+      <div className="flex mb-6">
+        <h1 className="text-3xl font-bold">Schedule</h1>
+      </div>
     </div>
   );
 }
