c2e216c8ba7b00f81fcbfb678a3a1a1df739a739
王哲玮 <m13636472690@163.com>
2026-01-17 17:47:30 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Page() {
   const router = useRouter();
 
   useEffect(() => {
-    const guide = localStorage.getItem("guideCompleted");
+    // const guide = localStorage.getItem("guideCompleted");
 
-    if (guide === "true") {
-      router.replace("/main/track");
-    } else {
-      router.replace("/guide");
-    }
+    // if (guide === "true") {
+    router.replace("/main/track");
+    // } else {
+    //   router.replace("/guide");
+    // }
   }, [router]);
 
   return null;
