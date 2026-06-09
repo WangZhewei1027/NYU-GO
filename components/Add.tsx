272f06b54e65d2924d4e3a6c8f8d7aaffcc941c9
王哲玮 <m13636472690@163.com>
2026-06-09 15:40:50 +0800

    Refactor Add component: simplify dialog structure, adjust styling, and remove unused elements

--- components/Add.tsx
+++ components/Add.tsx
@@ -5,11 +5,8 @@ import { MdOutlineAdd } from "react-icons/md";
 import { routesColor } from "@/app/utils/utils";
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogDescription,
-  DialogFooter,
-  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
@@ -23,7 +20,7 @@ function RouteItem({ route, promo }: { route: string; promo: string }) {
 
   return (
     <div
-      className={`w-full h-40 shadow rounded p-4 flex flex-col transition-all ${
+      className={`w-full h-32 min-[375px]:h-36 sm:h-40 shadow rounded p-3 sm:p-4 flex flex-col transition-all ${
         selected ? "bg-egg-blue-400 text-white" : "bg-white text-black"
       }`}
       onClick={() => toggleRoute(route)}
@@ -52,16 +49,10 @@ export default function Add() {
             </Button>
           </div>
         </DialogTrigger>
-        <DialogContent className="max-w-[90vw] max-h-[100vh] overflow-hidden p-4 rounded">
-          <DialogHeader>
-            <DialogTitle asChild>
-              <div className="hidden">Edit Routes</div>
-            </DialogTitle>
-            <DialogDescription asChild>
-              <div className="hidden">Edit Routes</div>
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid grid-cols-2 gap-2">
+        <DialogContent className="max-w-[90vw] max-h-[85svh] overflow-hidden flex flex-col px-4 py-5 rounded">
+          <DialogTitle className="sr-only">Edit Routes</DialogTitle>
+          <DialogDescription className="sr-only">Edit Routes</DialogDescription>
+          <div className="grid grid-cols-2 gap-3 overflow-y-auto">
             {Object.keys(routesColor).map((route, index) => {
               //console.log(route);
               return (
@@ -73,17 +64,6 @@ export default function Add() {
               );
             })}
           </div>
-          <DialogFooter className="relative">
-            <DialogClose asChild>
-              <Button
-                type="button"
-                variant="outline"
-                className="fixed h-10 w-20 bottom-12 left-1/2 transform -translate-x-1/2 border border-egg-blue-400 rounded-full text-egg-blue-400 text-base z-50"
-              >
-                Save
-              </Button>
-            </DialogClose>
-          </DialogFooter>
         </DialogContent>
       </Dialog>
     </>
